266e6c0046a16ba7d7fe97eb77ac4adee4368d0c
Jacob Simpson <jacobsimpsondev@gmail.com>
2025-04-04 13:12:07 -0400

    Initial Commit

---
+++ PersonalWebEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PersonalWebEditor
+//
+//  Created by jacob simpson on 4/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PersonalWebEditor/PersonalWebEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalWebEditorApp.swift
+//  PersonalWebEditor
+//
+//  Created by jacob simpson on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct PersonalWebEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
